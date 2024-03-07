39a03018d26875144245724277051c87a4c26435
MykolaSenko <mykola.senko@gmail.com>
2024-03-07 09:32:51 +0100

    working app with hyper parameters. Version 1.0

--- streamlit.py
+++ streamlit.py
@@ -16,6 +16,8 @@ st.markdown(
 
 uploaded_file = st.file_uploader('File uploader', type=["csv", "xlsx"])
 
+# This block of code is responsible for reading the uploaded file and displaying its contents in a
+# table within a Streamlit expander.
 if uploaded_file is not None:
     # Determine the file type
     if uploaded_file.name.endswith('.csv'):
@@ -26,64 +28,98 @@ if uploaded_file is not None:
         df = pd.read_excel(uploaded_file, engine='openpyxl')
     with st.expander("Show the table"):
         show_table(df)
+# `parameters = {}` is initializing an empty dictionary named `parameters`. This dictionary is used to
+# store advanced parameters that the user can select when making predictions using the NeuralProphet
+# model. The parameters are set based on the user's selections in the Streamlit interface, and they
+# are then passed as keyword arguments to the `np_model` function for making predictions. This allows
+# the user to customize the prediction process by specifying parameters such as normalization type,
+# learning rate, epochs, batch size, loss function, seasonality, and seasonality mode.
+parameters = {}
 
 col1, col2 = st.columns(2)
+# The code block `with col1:` is creating a column layout in the Streamlit interface where the
+# subsequent elements will be placed. Within this column layout, the code is displaying a radio button
+# using `st.radio` with the prompt 'Choose frequency:' and options 'daily' and 'weekly'.
 with col1:
     freq = st.radio('Choose frequency:', ['daily','weekly'])
     st.write(f"You have chosen frequency: {freq}")
 
+# The code block `with col2:` is creating a column layout in the Streamlit interface where the
+# subsequent elements will be placed. Within this column layout, the code is displaying a number input
+# field using `st.number_input` with the prompt 'Please specify the number of periods for which you
+# would like to make a prediction:', a minimum value of 0, and a step of 1. This input field allows
+# the user to specify the number of periods for which they would like to make a prediction using the
+# NeuralProphet model. The value entered by the user in this input field will be stored in the
+# variable `horiz`, which represents the prediction horizon.
 with col2:
     horiz = st.number_input('Please specify the number of periods for which you would like to make a prediction:', min_value=0, step=1)
     
+# The code block you provided is creating an expander in the Streamlit interface titled "Choose
+# Advanced Parameters" with a link to a guide on hyperparameter selection for NeuralProphet. Within
+# this expander, there are several interactive elements that allow the user to customize advanced
+# parameters for making predictions using the NeuralProphet model.
 with st.expander("Choose [Advanced Parameters](https://neuralprophet.com/how-to-guides/feature-guides/hyperparameter-selection.html)"):
     select_normal = st.checkbox("Check to select [normalize parameter](https://neuralprophet.com/code/forecaster.html)")
     if select_normal:
         normalize = st.selectbox("Choose the type of normalization to apply to the time series:", ['minmax (Default)', 'off', 'standardize', 'soft', 'soft1'])
         if normalize == 'minmax (Default)':
             normalize = 'minmax'
+        parameters['normalize'] = normalize
     select_learn = st.checkbox("Check to select [learning rate parameter](https://neuralprophet.com/code/forecaster.html). Auto by default.")
     if select_learn:
         learning_rate = st.number_input('Enter a learning rate between 0.01 and 10', min_value=0.01, max_value=10.00, step=0.01)
+        parameters['learning_rate'] = learning_rate
     select_epochs = st.checkbox("Check to select [epochs parameter](https://neuralprophet.com/code/forecaster.html). Auto by default.")
     if select_epochs:
         epochs = st.number_input('Enter a number of epochs between 5 and 500', min_value=5, max_value=500, step=1)
+        parameters['epochs'] = epochs
     select_batch_size = st.checkbox('Check to select [batch size parameter](https://neuralprophet.com/code/forecaster.html). Auto by default.')
     if select_batch_size:
         batch_size = st.number_input('Enter a batch size between 8 and 1024', min_value=8, max_value=1024, step=1)
+        parameters['batch_size'] = batch_size
     select_loss = st.checkbox("Check to select [loss function parameter](https://neuralprophet.com/code/forecaster.html)")
     if select_loss:
-        loss_func = st.selectbox("Choose the type of loss function parameter:", ['SmoothL1Loss (Default)', 'MSE', 'MAE', 'torch.nn.L1Loss'])
+        loss_func = st.selectbox("Choose the type of loss function parameter:", ['SmoothL1Loss (Default)', 'MSE', 'MAE'])
         if loss_func == 'SmoothL1Loss (Default)':
             loss_func = 'SmoothL1Loss'
+        parameters['loss_func'] = loss_func
     select_seasonality = st.checkbox("Check to select [seasonality parameter](https://neuralprophet.com/code/forecaster.html). Auto be default.")
     if select_seasonality:
         seasonality_options = ['yearly_seasonality', 'weekly_seasonality', 'daily_seasonality']
         selected_seasonality = st.multiselect("Choose the type of seasonality parameter:", seasonality_options)
         if 'yearly_seasonality' in selected_seasonality:
-            yearly_seasonality = True
+            parameters['yearly_seasonality'] = True
         if 'weekly_seasonality' in selected_seasonality:
-            weekly_seasonality = True
+            parameters['weekly_seasonality'] = True
         if 'daily_seasonality' in selected_seasonality:
-            daily_seasonality = True
+            parameters['daily_seasonality'] = True
     select_seasonality_mode = st.checkbox("Check to select [seasonality mode parameter](https://neuralprophet.com/code/forecaster.html)")
     if select_seasonality_mode:
         seasonality_mode = st.selectbox("Choose the type of loss function parameter:", ['additive (Default)', 'multiplicative'])
         if seasonality_mode == 'additive (Default)':
             seasonality_mode = 'additive'
+        parameters['seasonality_mode'] = seasonality_mode
 
+# This code snippet is checking if the variable `horiz` is not None and if its value is greater than
+# 0. If both conditions are met, it will display a button labeled 'Make NeuralProphet prediction'
+# using Streamlit's `st.button` function. This button allows the user to trigger the process of making
+# predictions using the NeuralProphet model based on the uploaded data and selected parameters.
 if horiz is not None and horiz > 0:
     make_prediction = st.button('Make NeuralProphet prediction')
 
+    # This block of code is responsible for handling the process of making predictions using the
+    # NeuralProphet model when the user clicks the 'Make NeuralProphet prediction' button in the
+    # Streamlit interface. Here's a breakdown of what each part of the code is doing:
     if make_prediction:
         with st.spinner(text='In progress'):
             try:
                 df_model = normal_df(df, freq)
-                df_pred = np_model(df_model, horiz)
-                normal_mae = norm_mae(df_pred, freq)
                 st.success('Done')
             except Exception as e:
                 st.warning(" Please upload the valid file", icon="⚠️")
                 pass
+            df_pred = np_model(df_model, horiz, **parameters)
+            normal_mae = norm_mae(df_pred, freq)
         try:
             fig = plot_data(df_pred)
             st.plotly_chart(fig, use_container_width=True)
@@ -95,7 +131,7 @@ if horiz is not None and horiz > 0:
         
             with st.expander("Show the table with predicted values"):
                 show_table(df_pred)
-            st.write("Prediction is made with these parameters:")
+            st.write("Prediction was made with these parameters:")
             st.write(f"Freaquency: {freq}.")
             st.write(f"Horizon: {horiz} periods.")
             if select_normal:
